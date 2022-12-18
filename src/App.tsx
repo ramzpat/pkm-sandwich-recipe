7537ea02600d490bce9a4347b5cb1c9cb42bb02d
Pattaravut Maleehuan <pattaravut.m@gmail.com>
2022-12-18 15:30:50 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {ingredient, sandwich_ingredients, find_candidate_ingredients, POWER_ALIAS} from './ts_scripts/search_recipe'
+
+import TypeList from './ts_scripts/data/types.json'
+import PowerList from './ts_scripts/data/powers.json'
 
 function App() {
-  const TypeList = ["Normal", "Fire", "Water", "Grass"];
+  // const TypeList = ["Normal", "Fire", "Water", "Grass"];
   const [typeFilter, setTypeFilter] = useState<string>("Normal");
 
-  const LevelList = [1,2,3];
+  const LevelList = [2,3];
   const [levelFilter, setLevelFilter] = useState<number>(2);
 
-  const PowerList = ["Sparkling", "Encounter", "Egg", "Raid"];
+  // const PowerList = ["Sparkling", "Encounter", "Egg", "Raid"];
   const [powerFilters, setPowerFilters] = useState<string[]>([PowerList[0]])
   const addPower = (power:string) => {
     if (powerFilters.length >= 3) {
@@ -21,7 +25,7 @@ function App() {
   const removePower = (power:string) => {
     setPowerFilters(
       powerFilters.filter(
-        (_power) => _power != power
+        (_power) => _power !== power
       )
     )
   }
@@ -32,15 +36,35 @@ function App() {
       addPower(power)
     }
   }
+  
+  const [useHerbal, setUseHerbal] = useState<boolean>(false);
+  const toggleUseHerbal = () => {
+    setUseHerbal(!useHerbal)
+  }
+
+  const [recipe, setRecipe] = useState<sandwich_ingredients | null>(null);
+
+  const prepare_recipe = () => {
+    const _recipe = find_candidate_ingredients(levelFilter, typeFilter, powerFilters, useHerbal);
+    setRecipe(_recipe)
+  }
+
+  useEffect(() => {
+    const _recipe = find_candidate_ingredients(levelFilter, typeFilter, powerFilters, useHerbal);
+    setRecipe(_recipe)
+  }
+  , [typeFilter, powerFilters, useHerbal])
+  
 
   return (
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
 
-        <div id="filter-container">
+        <div id="filter-container-id">
           <div className="filter-container">
             <span className="filter-title">Type: </span>
+            <div className="choice-container">
             {
               TypeList.map(
                 (type:string, index:number) => (
@@ -53,25 +77,30 @@ function App() {
                 )
               )
             }
+            </div>
           </div>
 
-          <div className="filter-container">
+          {/* <div className="filter-container">
             <div className="filter-title">Level: </div>
+
+            <div className="choice-container">
             {
               LevelList.map(
                 (level:number) => (
                 <div 
                   key={level}
-                  className={`level-box ${(levelFilter == level)?"level-select":"level"}`}
+                  className={`level-box ${(levelFilter === level)?"level-select":"level"}`}
                   onClick={() => { setLevelFilter(level) }}>
                 {level}
                 </div>
               ))
             }
-          </div>
+            </div>
+          </div> */}
 
           <div className="filter-container">
             <span className="filter-title">Powers: </span>
+            <div className="choice-container">
             {
               PowerList.map(
                 (power:string, index:number) => (
@@ -84,19 +113,45 @@ function App() {
                 )
               )
             }
+            </div>
+          </div>
+          <div className="filter-container">
+            <span className="filter-title">Herbal: </span>
+            <div className="herbal-container">
+            {
+              <div 
+              onClick={() => {toggleUseHerbal()}}
+              className={`power-box ${(useHerbal)?"power-select":"power-unselect"}`}>
+                {
+                `${(useHerbal)?"Use":"Unuse"}` 
+                }
+              </div>
+            }
+            </div>
           </div>
         </div>
 
         <div className="result-container">
+          <input type="button" value="Search" onClick={() =>{ prepare_recipe() }} />
           <div className="recipe-box">
-            <li>
-              <span className="ingredient">Hamberger: </span>
-              <span className="ingred-amont">2</span>
-            </li>
-            <li>
-              <span className="ingredient">Hamberger: </span>
-              <span className="ingred-amont">2</span>
-            </li>
+            {
+              recipe?.fillings.map(
+                (ingre:ingredient, index:number) => (
+                  <div key={index} className="ingredient">
+                    {ingre.name}
+                  </div>
+                )
+              )
+            }
+            {
+              recipe?.condiments.map(
+                (ingre:ingredient, index:number) => (
+                  <div key={index} className="ingredient">
+                    {ingre.name}
+                  </div>
+                )
+              )
+            }
           </div>
           
         </div>
